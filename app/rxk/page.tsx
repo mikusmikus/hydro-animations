5f71eb8d90ec36e41663000e50d2547d064673a4
designer-rxk <raivis.karklins@try.no>
2025-02-24 21:50:00 +0200

    Remove unused Modal and Buttons components from Home page

--- app/rxk/page.tsx
+++ app/rxk/page.tsx
@@ -1,12 +1,8 @@
-import Buttons from "./components/buttons"
-import Modal from "./components/modal"
 import Navigation from "./components/navigation"
 
 export default function Home() {
   return (
     <main className="h-[200dvh] bg-[#e1e1e1] relative">
-        {/* <Modal /> */}
-        {/* <Buttons /> */}
         <Navigation />
     </main>
   )
