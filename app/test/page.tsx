aee5b79ac79f71c74caa36d50fa20c980534d227
Mikus Meikulis <mikus.meikulis@giraffe360.com>
2025-07-04 10:45:52 +0300

    Refactor Test component to remove unstable cache and directly call getRandomNumber for random number generation

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 
 import { getRandomNumber } from "./randomNumberApi";
-import { unstable_cache } from "next/cache";
 
 export const dynamic = "force-static";
 
 export default async function Test() {
-    const randomNumber = await unstable_cache(getRandomNumber, ["randomNumber"], {
-        tags: ["randomNumber"],
-        revalidate: 30, // 10 seconds
-    })();
-    console.log("revalidating");
-
+    const randomNumber = await getRandomNumber();
     console.log(randomNumber);
     return <div>Test {randomNumber}</div>;
 }
