770ac7229bc9fe66dc8d4acb9b52cb30bbab4d97
Mikus Meikulis <mikus.meikulis@giraffe360.com>
2025-07-04 11:35:11 +0300

    Add Loading component and refactor Test component to use unstable_cache for random number generation

---
+++ app/test/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+    return <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-[red]/30 bg-opacity-50">Loading...</div>;
+}
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import { getRandomNumber } from "./randomNumberApi";
+import { unstable_cache } from "next/cache";
 
 export const dynamic = "force-static";
 
 export default async function Test() {
-    const randomNumber = await getRandomNumber();
+    const randomNumber = await unstable_cache(getRandomNumber, ["randomNumber"], {
+        tags: ["randomNumber"],
+        revalidate: 20,
+    })();
     console.log(randomNumber);
     return <div>Test {randomNumber}</div>;
 }
