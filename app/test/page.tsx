e60a7c7899699e01e480583155cc86fbe4898483
Mikus Meikulis <mikus.meikulis@giraffe360.com>
2025-07-09 09:22:12 +0300

    Add generateMetadata function to Test component for dynamic page title based on random number

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 import { getRandomNumber } from "./randomNumberApi";
 
+
 export const dynamic = "force-static";
 export const revalidate = 20;
+
+export const generateMetadata = async () => {
+    const randomNumber = await getRandomNumber();
+    return {
+        title: `Test ${randomNumber}`,
+    };
+};
 
 export default async function Test() {
     const randomNumber = await getRandomNumber();
